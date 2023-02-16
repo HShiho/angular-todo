8da1d00b419094bf8f031180a90c21ef62fed9a5
HShiho <108495008+HShiho@users.noreply.github.com>
2023-02-16 15:22:36 +0000

    [add]tasks

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  // title = 'angular-todo';
+  tasks = [
+    {title: '牛乳を買う', done: false},
+    {title: '可燃ごみを出す', done: true},
+    {title: '銀行に行く', done: false},
+  ];
+}
