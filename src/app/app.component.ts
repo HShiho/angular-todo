b162bf0fbabbf6b6af658c88da8f22417c4742db
HShiho <108495008+HShiho@users.noreply.github.com>
2023-03-23 03:57:07 +0000

    [add]api連携

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from '@reactivex/rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
 
@@ -18,9 +18,12 @@ export class AppComponent {
   constructor(private http: HttpClient) { }
 
   booksUrl = 'assets/books.json';
+  
+  
 
 getConfig() {
   return this.http.get<AppComponent>(this.booksUrl);
 }
+
 
 }
